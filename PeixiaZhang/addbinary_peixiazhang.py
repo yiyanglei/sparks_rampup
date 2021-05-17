3eeb95bdadc25e12e4615b404ab5eb6ded802417
PennyCoder <44801137+PennyCoder5704@users.noreply.github.com>
2021-05-17 14:09:10 +0800

    Update addbinary_peixiazhang.py

--- PeixiaZhang/addbinary_peixiazhang.py
+++ PeixiaZhang/addbinary_peixiazhang.py
@@ -1,18 +1,14 @@
-"""
-每周一题
-    两个二进制字符串（字符串非空且只包含数字1和0）,返回它们的和（用二进制表示）。
-示例 1:
-    输入: a = "11", b = "1"
-    输出: "100"
-示例 2:
-    输入: a = "1010", b = "1011"
-    输出: "10101"
-"""
+"""addBinary function"""
+
 class Solution:
-    def addBinary(self, a, b) -> str:
+    def addBinary(self, a, b):
         x, y = int(a, 2), int(b, 2)
         while y:
             answer = x ^ y
             carry = (x & y) << 1
             x, y = answer, carry
-        return bin(x)[2:]
+        print(bin(x)[2:])
+
+if __name__ == '__main__':
+    add = Solution()
+    add.addBinary("1010", "1011")
