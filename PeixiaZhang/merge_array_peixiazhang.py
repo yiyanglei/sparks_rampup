6da62854f8310bf6f46960f297050f450ee16286
PennyCoder <44801137+PennyCoder5704@users.noreply.github.com>
2021-05-25 11:07:43 +0800

    Update merge_array_peixiazhang.py

--- PeixiaZhang/merge_array_peixiazhang.py
+++ PeixiaZhang/merge_array_peixiazhang.py
@@ -1,15 +1,24 @@
 """Merge array and sort function"""
+
 from typing import List
 
+
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int):
+    """..."""
+
+    def merge(self, nums1: List[int], m: int, nums2: List[int]) -> List[int]:
+        """Define for merge two array and sort function.
+        :param nums1: int list array1
+        :param m: array1 number
+        :param nums2: int list array2
+        :return: int list
         """
-        Put array2 at the end of array1, and sort entire array.
-        """
+
         nums1[m:] = nums2
         nums1.sort()
-        print("Merge Result:", nums1)
+        return nums1
+
 
 if __name__ == '__main__':
     merge_array = Solution()
-    merge_array.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+    print(merge_array.merge([1,2,3,0,0,0], 3, [2,5,6]))
